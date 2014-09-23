7eb6246ffef22858b8e1ba0e0c4a406fae9fcaba
1ternal <kevinzyl@gmail.com>
2014-09-23 17:05:03 +0800

    Task and gen_tcp doc translate

--- README.md
+++ README.md
@@ -7,3 +7,7 @@ mix and otp
 + [Supervisor和Application](/getting_started/mix_and_otp/5.markdown)
 + [ETS](/getting_started/mix_and_otp/6.markdown)
 + [依赖和“雨伞”项目](/getting_started/mix_and_otp/7.markdown)
++ [任务与gen_tcp](/getting_started/mix_and_otp/8.markdown)
++ [任务与gen_tcp](/getting_started/mix_and_otp/8.markdown)
++ [文档、测试和管道符](/getting_started/mix_and_otp/9.markdown)
++ [分布任务与配置](/getting_started/mix_and_otp/10.markdown)
--- apps/kv_server/lib/kv_server.ex
+++ apps/kv_server/lib/kv_server.ex
@@ -1,19 +1,48 @@
 defmodule KVServer do
   use Application
 
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
     children = [
-      # Define workers and child supervisors to be supervised
-      # worker(KVServer.Worker, [arg1, arg2, arg3])
+      supervisor(Task.Supervisor, [[name: KVServer.TaskSupervisor]]),
+      worker(Task, [KVServer, :accept, [4040]])
     ]
 
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: KVServer.Supervisor]
     Supervisor.start_link(children, opts)
+  end
+
+  @doc """
+  开始在指定的端口上接受连接
+  """
+  def accept(port) do
+    {:ok, socket} = :gen_tcp.listen(port,
+                [:binary, packet: :line, active: false])
+    IO.puts "在#{port}端口接受连接"
+    loop_acceptor(socket)
+  end
+
+  defp loop_acceptor(socket) do
+    {:ok, client} = :gen_tcp.accept(socket)
+    Task.Supervisor.start_child(KVServer.TaskSupervisor, fn -> serve(client) end)
+    loop_acceptor(socket)
+  end
+
+  defp serve(client) do
+    client
+    |> read_line()
+    |> write_line(client)
+
+    serve(client)
+  end
+
+  defp read_line(socket) do
+    {:ok, data} = :gen_tcp.recv(socket,0)
+    data
+  end
+
+  defp write_line(line,socket) do
+    :gen_tcp.send(socket, line)
   end
 end
